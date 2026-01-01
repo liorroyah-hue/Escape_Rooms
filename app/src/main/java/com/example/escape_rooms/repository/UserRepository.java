6053c392e1c47074b11af8fdeb773fd4953aeda0
amielpeled <amielnoy@gmail.com>
2026-01-01 14:04:15 +0200

    refactor to get error from sapbase

--- app/src/main/java/com/example/escape_rooms/repository/UserRepository.java
+++ app/src/main/java/com/example/escape_rooms/repository/UserRepository.java
@@ -4,6 +4,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import com.example.escape_rooms.model.User;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -20,7 +21,9 @@ public class UserRepository {
     public static final String SUPABASE_URL="https://wjwbshqrvbgdtqanztqz.supabase.co";
     public static final String SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indqd2JzaHFydmJnZHRxYW56dHF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyOTA1MzgsImV4cCI6MjA3ODg2NjUzOH0.2LL7iEfi0gv_JAaR2984X2ybn4LclXGSTwR-9runIhM";
     private final OkHttpClient client = new OkHttpClient();
-    private final Gson gson = new Gson();
+    
+    // Explicitly configure Gson to NOT serialize nulls (this ensures 'id' is omitted)
+    private final Gson gson = new GsonBuilder().create();
 
     public interface UsersCallback<T> {
         void onSuccess(T result);
@@ -44,13 +47,15 @@ public class UserRepository {
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if(response.isSuccessful()) {
-                    String json = response.body().string();
-                    Type listType = new TypeToken<List<User>>() {}.getType();
-                    List<User> users = gson.fromJson(json, listType);
-                    callback.onSuccess(users);
-                } else {
-                    callback.onError(new Exception("Error fetching users: " + response.code()));
+                try (Response resp = response) {
+                    if(resp.isSuccessful()) {
+                        String json = resp.body().string();
+                        Type listType = new TypeToken<List<User>>() {}.getType();
+                        List<User> users = gson.fromJson(json, listType);
+                        callback.onSuccess(users);
+                    } else {
+                        callback.onError(new Exception("Fetch Error: " + resp.code()));
+                    }
                 }
             }
         });
@@ -59,6 +64,9 @@ public class UserRepository {
     public void addUser(User newUser, UsersCallback<User> callback){
         String url = SUPABASE_URL + "/rest/v1/User";
         String json = gson.toJson(newUser);
+        
+        Log.d("UserRepository", "Registering user with JSON: " + json);
+        
         RequestBody body = RequestBody.create(json, MediaType.parse("application/json"));
 
         Request request = new Request.Builder()
@@ -77,17 +85,21 @@ public class UserRepository {
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if(response.isSuccessful()){
-                    String jsonResponse = response.body().string();
-                    Type listType = new TypeToken<List<User>>(){}.getType();
-                    List<User> addedUsers = gson.fromJson(jsonResponse, listType);
-                    if(addedUsers != null && !addedUsers.isEmpty()){
-                        callback.onSuccess(addedUsers.get(0));
+                try (Response resp = response) {
+                    String responseBody = resp.body().string();
+                    if(resp.isSuccessful()){
+                        Type listType = new TypeToken<List<User>>(){}.getType();
+                        List<User> addedUsers = gson.fromJson(responseBody, listType);
+                        if(addedUsers != null && !addedUsers.isEmpty()){
+                            callback.onSuccess(addedUsers.get(0));
+                        } else {
+                            callback.onError(new Exception("Failed to parse response"));
+                        }
                     } else {
-                        callback.onError(new Exception("Failed to parse added user"));
+                        // Return the actual error message from Supabase (e.g. duplicate key, etc.)
+                        Log.e("UserRepository", "Supabase Error: " + responseBody);
+                        callback.onError(new Exception("Server Error: " + responseBody));
                     }
-                } else {
-                    callback.onError(new Exception("Error adding user: " + response.code()));
                 }
             }
         });
