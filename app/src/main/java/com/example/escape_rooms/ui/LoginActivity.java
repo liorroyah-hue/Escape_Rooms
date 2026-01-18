deaac95e31e93f23ca8197babffea93ebee78045
amielpeled <amielnoy@gmail.com>
2026-01-18 11:06:37 +0200

    rename refactor

--- app/src/main/java/com/example/escape_rooms/ui/LoginActivity.java
+++ app/src/main/java/com/example/escape_rooms/ui/LoginActivity.java
@@ -70,7 +70,7 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show();
                 
                 textStatus.postDelayed(() -> {
-                    Intent intent = new Intent(this, ChoosingGameVarient.class);
+                    Intent intent = new Intent(this, ChoosingGameVarientActivity.class);
                     startActivity(intent);
                     finish();
                 }, 1000);
